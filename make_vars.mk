bfc8199f8a01c5f0b079b641d04c350c5c7edf95
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-02 06:36:49 +0200

    initial

---
+++ make_vars.mk
@@ -0,0 +1,24 @@
+NAME =
+LIBNAME = lib$(NAME)
+GIT_REPO =
+
+CALL_DIR := $(shell pwd)
+SRC_DIR	:= $(ROOT)/sources
+OBJ_DIR := $(ROOT)/objects
+LIB_DIR := $(ROOT)/libs
+LIBS_L := $(LIB_DIR)/objects
+LIBS_I := $(LIB_DIR)/includes
+OUT_DIR_LIB := $(LIBS_L)
+OUT_DIR_H := $(LIBS_I)
+
+include $(SRC_DIR)/targets.mk
+DEPENDENCIES =
+
+CC := gcc
+CFLAGS_MORE =
+ifndef CFLAGS
+	CFLAGS := -Wall -Wextra -Werror
+endif
+CFLAGS += $(CFLAGS_MORE)
+
+UTEST_DIR := $(ROOT)/unit-tests
---
+++ make_vars_release.mk
@@ -0,0 +1,20 @@
+NAME =
+LIBNAME = lib$(NAME)
+
+CALL_DIR := $(shell pwd)
+SRC_DIR	:= $(ROOT)/
+OBJ_DIR := $(ROOT)/
+LIB_DIR := $(ROOT)/
+LIBS_L := $(LIB_DIR)/
+LIBS_I := $(LIB_DIR)/
+OUT_DIR_LIB := $(LIBS_L)
+OUT_DIR_H := $(LIBS_I)
+
+include $(SRC_DIR)/targets.mk
+
+CC := gcc
+CFLAGS_MORE =
+ifndef CFLAGS
+	CFLAGS := -Wall -Wextra -Werror
+endif
+CFLAGS += $(CFLAGS_MORE)
