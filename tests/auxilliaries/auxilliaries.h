93ff57b7de95be5796ff4aa97e7707be342ae418
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-04 04:09:05 +0200

    i

---
+++ tests/auxilliaries/auxilliaries.h
@@ -0,0 +1,10 @@
+#ifndef AUXILLIARIES_H
+# define AUXILLIARIES_H
+
+# include "inner.h"
+
+int
+	check_depth(
+		t_s_rbt *tree);
+
+#endif
---
+++ tests/auxilliaries/check_depth.c
@@ -0,0 +1,40 @@
+#include "auxilliaries.h"
+
+static
+int
+	is_good(
+		int depth, int *previous)
+{
+	if (*previous == -1)
+		*previous = depth;
+	return (depth == *previous);
+}
+
+static
+int
+	recurse(
+		t_s_rbtn *node, int depth, int *previous)
+{
+	int		ret;
+
+	if (!node)
+		return (is_good(depth, previous));
+	if (!(node->attr & RED))
+		depth++;
+	ret = 1;
+	if ((ret *= recurse(node->kin[e_left], depth, previous))
+		ret *= recurse(node->kin[e_right], depth, previous);
+	return (ret);
+}
+
+int
+	check_depth(
+		t_s_rbt *tree)
+{
+	int		depth;
+	int		ret;
+
+	depth = -1;
+	ret = recurse(tree->anchor, 0, &depth);
+	return (ret);
+}
