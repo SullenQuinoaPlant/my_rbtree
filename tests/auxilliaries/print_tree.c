6ccd7361a508100c3e31ae22524bca3d2d342442
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-10 01:44:19 +0200

    i

---
+++ tests/auxilliaries/print_tree.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "aux_inner.h"
+
+static int		printer(void* nope) {
+	static int	count;
+
+	(void)nope;
+	printf("%d ", count);
+	return (count++);
+}
+
+int				print_tree(t_s_rbt*p)
+{
+	int		r;
+
+	printf("printing tree: \n");
+	r = rbt_apply_inord(printer, (void*)p);
+}
